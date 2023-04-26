43d8ebc13ca595c1acbf1f5344e076770793ab27
hmadrigal <hfmad@hotmail.com>
2023-04-26 16:54:53 -0600

    add zeek help and env vars

---
+++ src/zeek-grpc-http2/env.sh
@@ -0,0 +1,5 @@
+export BROPATH=.:/usr/local/zeek/share/zeek:/usr/local/zeek/share/zeek/policy:/usr/local/zeek/share/zeek/site:/usr/local/zeek/share/zeek/builtin-plugins
+export BRO_PLUGIN_PATH=/usr/local/zeek/lib/zeek/plugins
+export ZEEKPATH=.:/usr/local/zeek/share/zeek:/usr/local/zeek/share/zeek/policy:/usr/local/zeek/share/zeek/site:/usr/local/zeek/share/zeek/builtin-plugins
+export ZEEK_PLUGIN_PATH=/usr/local/zeek/lib/zeek/plugins
+export PATH=/usr/local/zeek/bin:$PATH
