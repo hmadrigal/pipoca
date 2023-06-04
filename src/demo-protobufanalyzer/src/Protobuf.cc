fd043281dfc376d8ea717f0fa68c6c2637d58fc0
hmadrigal <hmadrigal@estudiantec.cr>
2023-06-04 16:47:54 -0400

    Setting up PLug in component properly

--- src/demo-protobufanalyzer/src/Protobuf.cc
+++ src/demo-protobufanalyzer/src/Protobuf.cc
@@ -1,138 +1,145 @@
 #include "Protobuf.h"
 
 namespace plugin
-{
-	namespace Demo_ProtobufAnalyzer
+	{
+namespace Demo_ProtobufAnalyzer
 	{
 
-		std::tuple<std::vector<ProtobufPart>, std::vector<u_char>>
-		Protobuf::DecodeProto(std::vector<u_char> data)
+Protobuf::Protobuf(zeek::RecordValPtr args, zeek::file_analysis::File* file)
+	: zeek::file_analysis::Analyzer(zeek::file_mgr->GetComponentTag("PROTOBUF"), std::move(args), file)
+	{
+	}
+
+std::tuple<std::vector<ProtobufPart>, std::vector<u_char>>
+Protobuf::DecodeProto(std::vector<u_char> data)
+	{
+
+	std::vector<ProtobufPart> parts;
+
+	BufferReader reader = BufferReader(data);
+
+	while ( reader.LeftBytes() > 0 )
 		{
+		reader.Checkpoint();
 
-			std::vector<ProtobufPart> parts;
+		uint64_t byteRangeStart = reader.GetOffset();
+		uint64_t byteRangeEnd = 0;
+		const auto [indexType, _] = reader.ReadVarint();
+		uint64_t type = indexType & 0x7;
+		uint64_t index = indexType >> 3;
+		std::vector<u_char> value;
 
-			BufferReader reader = BufferReader(data);
+		// uint64_t value = 0;
 
-			while (reader.LeftBytes() > 0)
+		if ( type == TYPES::VARINT )
 			{
-				reader.Checkpoint();
-
-				uint64_t byteRangeStart = reader.GetOffset();
-				uint64_t byteRangeEnd = 0;
-				const auto [indexType, _] = reader.ReadVarint();
-				uint64_t type = indexType & 0x7;
-				uint64_t index = indexType >> 3;
-				std::vector<u_char> value;
-
-				// uint64_t value = 0;
-
-				if (type == TYPES::VARINT)
-				{
-					const auto [number, data] = reader.ReadVarint();
-					value = data;
-				}
-				else if (type == TYPES::LENDELIM)
-				{
-					const auto [length, _] = reader.ReadVarint();
-					value = reader.ReadBuffer(length);
-				}
-				else if (type == TYPES::FIXED32)
-				{
-					value = reader.ReadBuffer(4);
-				}
-				else if (type == TYPES::FIXED64)
-				{
-					value = reader.ReadBuffer(8);
-				}
-				else
-				{
-					throw std::runtime_error("Unknown type");
-				}
-
-				byteRangeEnd = reader.GetOffset();
-
-				ProtobufPart part = {byteRangeEnd, byteRangeStart, index, type, value};
-				parts.push_back(part);
-
-				// if ( protobuf_string )
-				// 	{
-				// 	std::string str(buffer.begin(), buffer.end());
-				// 	zeek::event_mgr.Enqueue(protobuf_string, GetFile()->ToVal(),
-				// 	                        zeek::make_intrusive<zeek::StringVal>(str));
-				// 	}
+			const auto [number, data] = reader.ReadVarint();
+			value = data;
+			}
+		else if ( type == TYPES::LENDELIM )
+			{
+			const auto [length, _] = reader.ReadVarint();
+			value = reader.ReadBuffer(length);
+			}
+		else if ( type == TYPES::FIXED32 )
+			{
+			value = reader.ReadBuffer(4);
+			}
+		else if ( type == TYPES::FIXED64 )
+			{
+			value = reader.ReadBuffer(8);
+			}
+		else
+			{
+			throw std::runtime_error("Unknown type");
 			}
 
-			std::vector<u_char> leftOver = reader.ReadBuffer(reader.LeftBytes());
-			return std::make_tuple(parts, leftOver);
+		byteRangeEnd = reader.GetOffset();
+
+		ProtobufPart part = {byteRangeEnd, byteRangeStart, index, type, value};
+		parts.push_back(part);
+
+		// if ( protobuf_string )
+		// 	{
+		// 	std::string str(buffer.begin(), buffer.end());
+		// 	zeek::event_mgr.Enqueue(protobuf_string, GetFile()->ToVal(),
+		// 	                        zeek::make_intrusive<zeek::StringVal>(str));
+		// 	}
 		}
 
-		bool Protobuf::DeliverStream(const u_char *data, uint64_t len)
-		{
-			// Keeps the data in memory
-			for (uint64_t i = 0; i <= len; i++)
-			{
-				buffer.push_back(data[i]);
-			}
+	std::vector<u_char> leftOver = reader.ReadBuffer(reader.LeftBytes());
+	return std::make_tuple(parts, leftOver);
+	}
 
-			return true;
+bool Protobuf::DeliverStream(const u_char* data, uint64_t len)
+	{
+	// Keeps the data in memory
+	for ( uint64_t i = 0; i <= len; i++ )
+		{
+		buffer.push_back(data[i]);
 		}
 
-		void Protobuf::DecodeProtobufPart(ProtobufPart part)
+	return true;
+	}
+
+void Protobuf::DecodeProtobufPart(ProtobufPart part)
+	{
+	switch ( part.type )
 		{
-			switch (part.type)
+		case TYPES::VARINT:
+			break;
+		case TYPES::LENDELIM:
 			{
-			case TYPES::VARINT:
-				break;
-			case TYPES::LENDELIM:
-			{
-				const auto [parts, leftOver] = DecodeProto(part.value);
-				if (part.value.size() > 0 && leftOver.size() == 0)
+			const auto [parts, leftOver] = DecodeProto(part.value);
+			if ( part.value.size() > 0 && leftOver.size() == 0 )
 				{
-					// part.value is likely to be a sub message
-					ProtobufDisplay(parts);
+				// part.value is likely to be a sub message
+				ProtobufDisplay(parts);
 				}
-				else
+			else
 				{
-					// part.value is likely to be a string or bytes
-					// trigger event using value
-					const u_char *data = part.value.data();
-					zeek::event_mgr.Enqueue(protobuf_string, GetFile()->ToVal(),
-											zeek::make_intrusive<zeek::StringVal>(new zeek::String(data, part.value.size(), false)));
-				}
-				break;
-			}
-			case TYPES::FIXED32:
+				// part.value is likely to be a string or bytes
 				// trigger event using value
-				break;
-			case TYPES::FIXED64:
-				// trigger event using value
-				break;
-			default:
-				throw std::runtime_error("Unknown type");
+				const u_char* data = part.value.data();
+				zeek::event_mgr.Enqueue(protobuf_string, GetFile()->ToVal(),
+				                        zeek::make_intrusive<zeek::StringVal>(
+											new zeek::String(data, part.value.size(), false)));
+				}
+			break;
 			}
+		case TYPES::FIXED32:
+			// trigger event using value
+			break;
+		case TYPES::FIXED64:
+			// trigger event using value
+			break;
+		default:
+			throw std::runtime_error("Unknown type");
 		}
+	}
 
-		bool Protobuf::EndOfFile()
+bool Protobuf::EndOfFile()
+	{
+
+	const auto [parts, _] = DecodeProto(buffer);
+
+	return ProtobufDisplay(parts);
+	}
+
+bool Protobuf::ProtobufDisplay(std::vector<ProtobufPart> parts)
+	{
+	for ( ProtobufPart part : parts )
 		{
-
-			const auto [parts, _] = DecodeProto(buffer);
-
-			return ProtobufDisplay(parts);
+		DecodeProtobufPart(part);
 		}
+	return true;
+	}
 
-		bool Protobuf::ProtobufDisplay(std::vector<ProtobufPart> parts)
-		{
-			for (ProtobufPart part : parts)
-			{
-				DecodeProtobufPart(part);
-			}
-			return true;
-		}
-
-		zeek::file_analysis::Analyzer *Protobuf::Instantiate(zeek::RecordValPtr args, zeek::file_analysis::File *file)
-		{
-			return new Protobuf(std::move(args), file);
-		}
+zeek::file_analysis::Analyzer* Protobuf::Instantiate(zeek::RecordValPtr args,
+                                                     zeek::file_analysis::File* file)
+	{
+	return new Protobuf(std::move(args), file);
+	}
 
 	}
-}
+	}
--- src/demo-protobufanalyzer/src/Protobuf.h
+++ src/demo-protobufanalyzer/src/Protobuf.h
@@ -1,61 +1,62 @@
 #pragma once
 
+#include <zeek/Event.h>
+#include <zeek/Val.h>
+#include <zeek/ZeekString.h>
+#include <zeek/file_analysis/Analyzer.h>
+#include <zeek/file_analysis/Component.h>
+#include <zeek/file_analysis/File.h>
+#include <zeek/file_analysis/Manager.h>
 #include <tuple>
 #include <vector>
 
 #include "BufferReader.h"
 #include "events.bif.h"
 
-#include <zeek/Event.h>
-#include <zeek/ZeekString.h>
-#include <zeek/Val.h>
-#include <zeek/file_analysis/Analyzer.h>
-#include <zeek/file_analysis/Component.h>
-#include <zeek/file_analysis/File.h>
-
 namespace plugin
-{
-	namespace Demo_ProtobufAnalyzer
+	{
+namespace Demo_ProtobufAnalyzer
 	{
 
-		typedef struct
-		{
-			uint64_t byteRangeEnd;
-			uint64_t byteRangeStart;
-			uint64_t index;
-			uint64_t type;
-			std::vector<u_char> value;
+typedef struct
+	{
+	uint64_t byteRangeEnd;
+	uint64_t byteRangeStart;
+	uint64_t index;
+	uint64_t type;
+	std::vector<u_char> value;
 
-		} ProtobufPart;
+	} ProtobufPart;
 
-		enum TYPES
-		{
-			VARINT = 0,
-			FIXED64 = 1,
-			LENDELIM = 2,
-			FIXED32 = 5
-		};
+enum TYPES
+	{
+	VARINT = 0,
+	FIXED64 = 1,
+	LENDELIM = 2,
+	FIXED32 = 5
+	};
 
-		class Protobuf : public zeek::file_analysis::Analyzer
-		{
-		public:
-			static zeek::file_analysis::Analyzer *Instantiate(zeek::RecordValPtr args, zeek::file_analysis::File *file);
+class Protobuf : public zeek::file_analysis::Analyzer
+	{
+public:
+	static zeek::file_analysis::Analyzer* Instantiate(zeek::RecordValPtr args,
+	                                                  zeek::file_analysis::File* file);
 
-			bool DeliverStream(const u_char *data, uint64_t len) override;
+	bool DeliverStream(const u_char* data, uint64_t len) override;
 
-			bool EndOfFile() override;
+	bool EndOfFile() override;
 
-			std::tuple<std::vector<ProtobufPart>, std::vector<u_char>>
-			DecodeProto(std::vector<u_char> data);
-			void DecodeProtobufPart(ProtobufPart part);
+	std::tuple<std::vector<ProtobufPart>, std::vector<u_char>>
+	DecodeProto(std::vector<u_char> data);
+	void DecodeProtobufPart(ProtobufPart part);
 
-			bool ProtobufDisplay(std::vector<ProtobufPart> parts);
+	bool ProtobufDisplay(std::vector<ProtobufPart> parts);
 
-		protected:
-			Protobuf(zeek::RecordValPtr args, zeek::file_analysis::File *file);
+protected:
+	Protobuf(zeek::RecordValPtr args, zeek::file_analysis::File* file);
 
-		private:
-			std::vector<u_char> buffer;
-		};
+private:
+	std::vector<u_char> buffer;
+	};
 	}
-}
+	}
