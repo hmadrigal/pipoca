eb6e58e845a94232545500329785b96b0490f460
hmadrigal <hmadrigal@estudiantec.cr>
2023-06-26 14:29:20 -0400

    Changed build process

--- src/test-app/build-bin.sh
+++ src/test-app/build-bin.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
+
+set -ex
+
 # check if server binary is already built
-if [ ! -f ./bin/GrpcTodoServer ]; then
+if [ ! -f ./bin/GrpcTodoServer/GrpcTodoServer ]; then
     # build server binary
-    mkdir -p ./bin
-    BIN_DIR=$(cd ./bin && pwd)
-    cd ../grpc-apps/dotnet/GrpcTodo
-    dotnet publish GrpcTodo.sln  --sc -c Release -o $BIN_DIR
+    mkdir -p ./bin/GrpcTodoServer
+    BIN_DIR=$(cd ./bin/GrpcTodoServer && pwd)
+    cd ../grpc-apps/dotnet/GrpcTodo/GrpcTodoServer
+    dotnet publish --sc -c Release -o $BIN_DIR
+    cd $OLDPWD
+fi
+
+# check if client binary is already built
+if [ ! -f ./bin/GrpcTodoClient/GrpcTodoClient ]; then
+    # build client binary
+    mkdir -p ./bin/GrpcTodoClient
+    BIN_DIR=$(cd ./bin/GrpcTodoClient && pwd)
+    cd ../grpc-apps/dotnet/GrpcTodo/GrpcTodoClient
+    dotnet publish --sc -c Release -o $BIN_DIR
+    cd $OLDPWD
 fi
