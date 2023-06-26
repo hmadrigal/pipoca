594e8cd3859d4fcf4831f20342a644666429b14b
hmadrigal <hmadrigal@estudiantec.cr>
2023-06-26 15:33:22 -0400

    capturing traffic for a given file

--- src/test-app/run-tests.sh
+++ src/test-app/run-tests.sh
@@ -8,6 +8,25 @@ if [ -z "$GRPC_TODO_SERVER" ]; then
     exit 1
 fi
 
+if [[ $EUID > 0 ]]; then
+  echo "Please run as root/sudo in order to capture packets."
+  exit 1
+fi
+
+# Build the binary
 ./build-bin.sh
 
+# Start tcpdump in the background
+tcpdump -i ens33 -s 0 -w owap-positive-waf-matches.pcap port 80 &
+TCPDUMP_PID=$!
+
+# Wait for tcpdump to start
+sleep 1
+
+# Starts tests
 python3 main.py
+
+# Kill tcpdump
+kill $TCPDUMP_PID
+
+
