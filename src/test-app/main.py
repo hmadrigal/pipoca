317794c05790ae50ab89dc1b86415c6c1a9b8f2c
hmadrigal <hmadrigal@estudiantec.cr>
2023-06-26 15:19:27 -0400

    refactor

--- src/test-app/main.py
+++ src/test-app/main.py
@@ -4,6 +4,17 @@ import sys
 import os
 import shlex
 
+def exec_todo_create_with(payload_file, server_url):
+    line_counter = 0
+
+    with open(payload_file, 'r') as f:
+        for line in f:
+            title = shlex.quote(line)
+            command = f'./bin/GrpcTodoClient/GrpcTodoClient create --server {server_url} --title "Title {line_counter} - {title}" --description "Description {line_counter}"'
+            print(f'Executing command: {command}')
+            os.system(command)
+
+
 def main():
 
     # check server variable GRPC_TODO_SERVER is set
@@ -12,14 +23,9 @@ def main():
         sys.exit(1)
 
     # reads a text file and iterates over each line
-    line_counter = 0
-    server_url=os.environ['GRPC_TODO_SERVER']
-    with open('./payloads/owap-positive-waf-matches.txt', 'r') as f:
-        for line in f:
-            title = shlex.quote(line)
-            command = f'./bin/GrpcTodoClient/GrpcTodoClient create --server {server_url} --title "Title {line_counter} - {title}" --description "Description {line_counter}"'
-            print(f'Executing command: {command}')
-            os.system(command)
+    server_url = os.environ['GRPC_TODO_SERVER']
+    payload_file = './payloads/owap-positive-waf-matches.txt'
+    command = exec_todo_create_with(payload_file, server_url)
 
 
 if __name__ == '__main__':
