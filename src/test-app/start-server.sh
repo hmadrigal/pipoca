a7ccf8fc16e6df7a316745114763b4ad13158628
hmadrigal <hmadrigal@estudiantec.cr>
2023-06-27 16:45:04 -0400

    allow trafic capture and logs during sever session

--- src/test-app/start-server.sh
+++ src/test-app/start-server.sh
@@ -8,7 +8,28 @@ if [[ $EUID > 0 ]]; then
   exit 1
 fi
 
+echo "Server Address: "
 ip addr
 
+echo ""
+
+
+# Start tcpdump in the background
+filename="GrpcTodoServer-$(date +%Y%m%d%H%M%S)"
+pcap_file="${filename}.cap"
+log_file="${filename}.log"
+tcpdump -i ens33 -s 0 -w "./bin/GrpcTodoServer/${pcap_file}" port 80 &
+TCPDUMP_PID=$!
+
+# Start the server
+echo "Starting GrpcTodoServer, see ${log_file} for details"
+echo "Press Ctrl+C to stop the server"
+echo "$(date) Starting GrpcTodoServer" >> "./bin/GrpcTodoServer/${log_file}"
+
 cd ./bin/GrpcTodoServer/
-ASPNETCORE_URLS=${ASPNETCORE_URLS:-"http://*:80"} ./GrpcTodoServer
+ASPNETCORE_URLS=${ASPNETCORE_URLS:-"http://*:80"} ./GrpcTodoServer >> "${log_file}"
+
+echo "$(date) GrpcTodoServer stopped" >> "${log_file}"
+
+# Kill tcpdump
+kill $TCPDUMP_PID
