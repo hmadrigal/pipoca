5469b508768f536900771cf1418b17f0bd953559
hmadrigal <hmadrigal@estudiantec.cr>
2023-07-08 09:45:38 -0400

    Allowing test to run inbatch

--- src/test-app/main.py
+++ src/test-app/main.py
@@ -17,12 +17,27 @@ def exec_todo_create_with(payload_file, server_url):
 
             line_counter += 1
 
-            title = shlex.quote(f"Title {line_counter} - {line}")
+            # if GRPC_TODO_TEST_CLEAN is set to true, use the payload as title
+            if os.environ.get('GRPC_TODO_TEST_CLEAN', '').lower() == 'true':
+                title = shlex.quote(line)
+            else:
+                title = shlex.quote(f"Title {line_counter} - {line}")
+            
             command = f'./bin/GrpcTodoClient/GrpcTodoClient create --server {server_url} --title {title} --description "Description {line_counter}"'
             print(f'Executing command: {command}')
             os.system(command)
 
     print (f'Executed {line_counter} commands using {payload_file}.')
+
+def exec_batch_create_with(payload_file, server_url):
+    
+    format="{0}"
+    if os.environ.get('GRPC_TODO_TEST_CLEAN', '').lower() != 'true':
+        format="Title {0}"
+    log = f'./traces/{os.path.basename(payload_file)}.log'
+    command = f'./bin/GrpcTodoClient/GrpcTodoClient create-batch --server {server_url} --filepath {shlex.quote(payload_file)} --format {shlex.quote(format)} > {log}'
+    print(f'Executing command: {command}')
+    os.system(command)
 
 def main():
 
@@ -39,8 +54,11 @@ def main():
     # reads a text file and iterates over each line
     server_url = os.environ['GRPC_TODO_SERVER']
     payload_file = os.environ['GRPC_TODO_PAYLOAD_FILE'] # './payloads/owap-positive-waf-matches.txt'
-    command = exec_todo_create_with(payload_file, server_url)
-
+    
+    if os.environ.get('GRPC_TODO_TEST_BATCH', '').lower() == 'true':
+        exec_batch_create_with(payload_file, server_url)
+    else:
+        exec_todo_create_with(payload_file, server_url)
 
 if __name__ == '__main__':
     main()
